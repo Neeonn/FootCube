09393f4b88b1b76e0696d7c9b50a750faf01c888
neon <srdjanneon@gmail.com>
2025-12-30 23:01:36 +0100

    patch: Add remaining banned time

--- src/main/java/io/github/divinerealms/footcube/matchmaking/ban/BanManager.java
+++ src/main/java/io/github/divinerealms/footcube/matchmaking/ban/BanManager.java
@@ -1,23 +1,38 @@
 package io.github.divinerealms.footcube.matchmaking.ban;
 
-import lombok.Getter;
-import org.bukkit.entity.Player;
+import static io.github.divinerealms.footcube.configs.Lang.BAN_REMAINING;
 
+import io.github.divinerealms.footcube.core.FCManager;
+import io.github.divinerealms.footcube.managers.Utilities;
+import io.github.divinerealms.footcube.utils.Logger;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import lombok.Getter;
+import org.bukkit.entity.Player;
 
 public class BanManager {
+
+  private final Logger logger;
   @Getter
   private final Map<UUID, Long> bannedPlayers = new ConcurrentHashMap<>();
+
+  public BanManager(FCManager fcManager) {
+    this.logger = fcManager.getLogger();
+  }
 
   public boolean isBanned(Player player) {
     if (!bannedPlayers.containsKey(player.getUniqueId())) {
       return false;
     }
+
+    long now = System.currentTimeMillis();
     long banTime = bannedPlayers.get(player.getUniqueId());
-    if (System.currentTimeMillis() < banTime) {
-      // TODO: Add a message to the player informing them about the remaining ban time.
+
+    if (now < banTime) {
+      long secondsLeft = (banTime - now) / 1000L;
+      logger.send(player, BAN_REMAINING, player.getDisplayName(),
+          Utilities.formatTime(secondsLeft));
       return true;
     }
 
